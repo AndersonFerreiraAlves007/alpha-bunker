fb2377c032817ef183799272f2c6c957823d25f2
Jaime <jaume.lan@gmail.com>
2022-07-25 20:39:59 -0300

    começo da navbar

--- web/src/components/Navbar/index.tsx
+++ web/src/components/Navbar/index.tsx
@@ -1,22 +1,34 @@
-import {Bank } from 'phosphor-react'
-import { ArrowsLeftRight} from 'phosphor-react';
-import { DownloadSimple} from 'phosphor-react';
-import { UploadSimple} from 'phosphor-react';
+import { Bank } from 'phosphor-react';
+import { ArrowsLeftRight } from 'phosphor-react';
+import { DownloadSimple } from 'phosphor-react';
+import { UploadSimple } from 'phosphor-react';
 
 const Navbar = () => {
   return (
-    <nav className="navbar flex">
-      <div className="bg-blue-400">
-        <Bank alt="extrato" />
+    <nav className="navbar flex gap-x-2.5 w-71">
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-blue-400 flex justify-center items-center w-14 h-12 rounded">
+          <Bank size={32} color="white" alt="extrato" />
+        </div>
+        <span>Extrato</span>
       </div>
-      <div className="bg-blue-400">
-        <ArrowsLeftRight alt="transferência" />
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-blue-400 flex justify-center items-center w-14 h-12 rounded">
+          <ArrowsLeftRight size={32} color="white" alt="transferência" />
+        </div>
+        <span>Transferir</span>
       </div>
-      <div className="bg-blue-400">
-        <DownloadSimple alt="depósito" />
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-blue-400 flex justify-center items-center w-14 h-12 rounded">
+          <DownloadSimple size={32} color="white" alt="depósito" />
+        </div>
+        <span>Depositar</span>
       </div>
-      <div className="bg-blue-400">
-        <UploadSimple alt="saque" />
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-blue-400 flex justify-center items-center w-14 h-12 rounded">
+          <UploadSimple size={32} color="white" alt="saque" />
+        </div>
+        <span>Sacar</span>
       </div>
     </nav>
   );
