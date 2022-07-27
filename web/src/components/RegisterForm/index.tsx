4982fcff795e8b14bad360e359e47f46009bcbe7
Jaime <jaume.lan@gmail.com>
2022-07-27 10:19:57 -0300

    fix: validações register form

--- web/src/components/RegisterForm/index.tsx
+++ web/src/components/RegisterForm/index.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import CustomForm from '../CustomForm';
 import { RegisterFormProps } from '../../@types/components';
 import { dataValidators } from '../../utils/register-form.validator';
+import { INPUT_TYPE_CLASSES } from '../Input';
 
 const ControlledRegisterForm = () => {
   const [formValues, setFormValues] = useState<RegisterFormProps>({
@@ -13,9 +14,27 @@ const ControlledRegisterForm = () => {
     password: '',
     confirmPassword: '',
   });
+  const [inputClass, setInputClass] = useState({
+    name: INPUT_TYPE_CLASSES.base,
+    birthdate: INPUT_TYPE_CLASSES.base,
+    email: INPUT_TYPE_CLASSES.base,
+    cpf: INPUT_TYPE_CLASSES.base,
+    password: INPUT_TYPE_CLASSES.base,
+    confirmPassword: INPUT_TYPE_CLASSES.base,
+  });
+  const [errorMessage, setErrorMessage] = useState({
+    name: '',
+    birthdate: '',
+    email: '',
+    cpf: '',
+    password: '',
+    confirmPassword: '',
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+    setInputClass({ ...inputClass, [name]: INPUT_TYPE_CLASSES.base });
+    setErrorMessage({ ...errorMessage, [name]: '' });
     setFormValues({ ...formValues, [name]: value });
   };
 
@@ -25,58 +44,71 @@ const ControlledRegisterForm = () => {
       type: 'text',
       value: formValues.name,
       onChange: handleChange,
-      label: 'Nome',
+      inputClassName: inputClass.name,
       placeholder: 'Digite seu Nome',
+      errorMessage: errorMessage.name,
     },
     {
       name: 'email',
       type: 'email',
       value: formValues.email,
       onChange: handleChange,
-      label: 'Email',
+      inputClassName: inputClass.email,
       placeholder: 'Digite seu Email',
+      errorMessage: errorMessage.email,
     },
     {
       name: 'birthdate',
       type: 'date',
       value: formValues.birthdate,
       onChange: handleChange,
-      label: 'Data de nascimento',
+      inputClassName: inputClass.birthdate,
       placeholder: 'Data de nascimento',
+      errorMessage: errorMessage.birthdate,
     },
     {
       name: 'cpf',
       type: 'text',
       value: formValues.cpf,
       onChange: handleChange,
-      label: 'Digite seu CPF',
+      inputClassName: inputClass.cpf,
       placeholder: 'CPF',
+      errorMessage: errorMessage.cpf,
     },
     {
       name: 'password',
       type: 'password',
       value: formValues.password,
       onChange: handleChange,
-      label: 'Digite sua Senha',
+      inputClassName: inputClass.password,
       placeholder: 'Senha',
+      errorMessage: errorMessage.password,
     },
     {
       name: 'confirmPassword',
       type: 'password',
       value: formValues.confirmPassword,
       onChange: handleChange,
-      label: 'Confirmae sua senha',
+      inputClassName: inputClass.confirmPassword,
       placeholder: 'Confirmar senha',
+      errorMessage: errorMessage.confirmPassword,
     },
   ];
+
+
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     const errors = dataValidators(formValues);
     if (Object.values(errors).some((error) => error)) {
-      console.log(errors);
+      Object.entries(errors).forEach(([key, value]) => {
+        if (value !== '') {
+          setFormValues({ ...formValues, [key]: '' });
+          setErrorMessage({...errorMessage, [key]: value.split('|')[0]});
+          setInputClass({...inputClass, [key] : INPUT_TYPE_CLASSES.error});
+        }
+      });
     }
-    console.log('Formulário enviado com sucesso!');
   };
 
   return (
